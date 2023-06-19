00aa24099cb01e6e757a891c88cfd3908e61c51d
paulcjy <paulcjy@naver.com>
2023-06-20 02:51:35 +0900

    testing generateStaticParams with constants

--- app/blog/[board]/page.tsx
+++ app/blog/[board]/page.tsx
@@ -4,7 +4,13 @@ import Link from 'next/link';
 
 export function generateStaticParams() {
   const staticParams = getStaticParams();
-  return staticParams.board;
+  // return staticParams.board;
+  return [
+    { board: 'github' },
+    { board: 'pages' },
+    { board: 'pc' },
+    { board: 'linux' },
+  ];
 }
 
 const fetchBoard = async (board: string) => {
@@ -15,21 +21,23 @@ export default async function Page({ params }: { params: { board: string } }) {
   const boardData: any = await fetchBoard(params.board);
   const [board] = await Promise.all([boardData]);
 
-  return (
-    <div>
-      {board.posts.map((post: any) => {
-        return (
-          <Link key={post.file} href={`/blog/${params.board}/${post.file}`}>
-            <PostBlock
-              title={post.data.title}
-              description={post.data.description}
-              date={post.data.date}
-            />
-          </Link>
-        );
-      })}
-      {/* <div>{board.category}</div>
-      <div>{board.count}</div> */}
-    </div>
-  );
+  // return (
+  //   <div>
+  //     {board.posts.map((post: any) => {
+  //       return (
+  //         <Link key={post.file} href={`/blog/${params.board}/${post.file}`}>
+  //           <PostBlock
+  //             title={post.data.title}
+  //             description={post.data.description}
+  //             date={post.data.date}
+  //           />
+  //         </Link>
+  //       );
+  //     })}
+  //     {/* <div>{board.category}</div>
+  //     <div>{board.count}</div> */}
+  //   </div>
+  // );
+
+  return <div>this is board: {params.board}</div>;
 }
